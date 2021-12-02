ba4f1435186027f195b1f8c65ef24e5767208796
Nathan Plouvier <naplouvi@student.le-101.fr>
2021-12-02 01:31:57 +0100

    ✨ add both solutions

--- 2020/1/main.go
+++ 2020/1/main.go
@@ -16,6 +16,37 @@ func toInteger(str string) int {
 	return intValue
 }
 
+func part1(arr []string) int {
+	for i := 0; i < len(arr); i++ {
+		a := toInteger(arr[i])
+		for j := 0; j < len(arr); j++ {
+			b := toInteger(arr[j])
+			if a+b == 2020 {
+				return a * b
+			}
+		}
+	}
+
+	panic(`No solution has been found.`)
+}
+
+func part2(arr []string) int {
+	for i := 0; i < len(arr); i++ {
+		a := toInteger(arr[i])
+		for j := 0; j < len(arr); j++ {
+			b := toInteger(arr[j])
+			for k := 0; k < len(arr); k++ {
+				c := toInteger(arr[k])
+				if a+b+c == 2020 {
+					return a * b * c
+				}
+			}
+		}
+	}
+
+	panic(`No solution has been found.`)
+}
+
 func main() {
 	data, err := os.ReadFile("input.txt")
 	if err != nil {
@@ -24,17 +55,6 @@ func main() {
 
 	arr := strings.Split(string(data), "\n")
 
-	for i := 0; i < len(arr); i++ {
-		a := toInteger(arr[i])
-		for j := 0; j < len(arr); j++ {
-			b := toInteger(arr[j])
-			for k := 0; k < len(arr); k++ {
-				c := toInteger(arr[k])
-				if a+b+c == 2020 {
-					fmt.Printf("Found: %d\n", a*b*c)
-					return
-				}
-			}
-		}
-	}
+	fmt.Printf("Part 1 solution: %d\n", part1(arr))
+	fmt.Printf("Part 2 solution: %d\n", part2(arr))
 }
